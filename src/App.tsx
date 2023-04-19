990650bd60b90446fd5c2e01ab263990e6f80242
JureKotnik <jure2002k@gmail.com>
2023-04-19 08:42:19 +0200

    Handling state

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+interface IState{
+  people:{
+    name: string
+    age: number
+    url: string
+    note?: string
+  }[]
+}
+
 function App() {
+
+  const[people, sePeople] = useState<IState["people"]>([])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>People invited to my party</h1>
     </div>
   );
 }
