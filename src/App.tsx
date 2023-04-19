c43a93249588cf65984ff2889d8719a0dabf2690
JureKotnik <jure2002k@gmail.com>
2023-04-19 09:17:35 +0200

    Handling Functions

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,14 @@ interface IState{
 
 function App() {
 
-  const[people, sePeople] = useState<IState["people"]>([])
+  const[people, sePeople] = useState<IState["people"]>([
+    {
+      name: "LeBron James",
+      url: "https://upload.wikimedia.org/wikipedia/commons/b/bf/LeBron_James_-_51959723161_%28cropped%29.jpg",
+      age: 36,
+      note: "Allergic to staying on the same team"
+    }
+  ])
 
   return (
     <div className="App">
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -9,11 +9,28 @@ interface IProps{
     }[]
   }
 
-const List: React.FC<IProps> = ({ people }) =>{
-    return(
-        <div>
-            I am a list
-        </div>
+const List: React.FC<IProps> = ({ people }) => {
+
+    const renderList = (): JSX.Element[] =>{
+        return people.map((person) => {
+            return(
+                <li className="List">
+                <div className="List-header">
+                    <img className="List-img" src={person.url} />
+                    <h2>{person.name}</h2>
+                </div>
+                <p>{person.age} years old</p>
+                <p className="List-note">{person.note}</p>
+            </li>
+            )
+        })
+    }
+
+
+    return (
+        <ul>
+            {renderList()} 
+        </ul>
     )
 }
 
